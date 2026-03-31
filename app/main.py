d704eaaa87acdd9ca33460f87069c79b39882458
lilajoha29 <lilajoha2907@gmail.com>
2026-03-30 21:33:52 -0500

    mejora la configuración de CORS en la aplicación y asegura el formato del archivo requirements.txt

--- app/main.py
+++ app/main.py
@@ -1,8 +1,19 @@
 from flask import Flask
+from flask_cors import CORS
 from interfaces.api.routes import api
 
 def create_app():
     app = Flask(__name__)
+    CORS(app, resources={
+        r"/*": {
+            "origins": [
+                "http://127.0.0.1:5500",
+                "https://polimarket-u9d6.onrender.com",
+                "https://entrega1-poli.vercel.app"
+            ]
+        }
+    })
+
     app.register_blueprint(api)
     return app
 
